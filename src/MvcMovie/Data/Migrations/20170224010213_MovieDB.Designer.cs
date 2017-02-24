9f5a36fc8a107b770bf4826106f49fbe31db8eb3
TVDOMAIN\PetSjo <peter.sjostrand@tekniskaverken.se>
2017-02-24 16:47:07 +0100

    Ignore and cs files added

--- src/MvcMovie/Data/Migrations/20170224010213_MovieDB.Designer.cs
+++ src/MvcMovie/Data/Migrations/20170224010213_MovieDB.Designer.cs
@@ -8,9 +8,10 @@ using MvcMovie.Data;
 namespace MvcMovie.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170224010213_MovieDB")]
+    partial class MovieDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
