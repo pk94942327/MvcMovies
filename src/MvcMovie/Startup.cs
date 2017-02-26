21e01535ec1170026d1db5e1ec1557a356aa6381
TVDOMAIN\PetSjo <peter.sjostrand@tekniskaverken.se>
2017-02-26 13:49:22 +0100

    Searching for Titles added.

--- src/MvcMovie/Startup.cs
+++ src/MvcMovie/Startup.cs
@@ -83,6 +83,7 @@ namespace MvcMovie
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            SeedData.Initialize(app.ApplicationServices);
         }
     }
 }
