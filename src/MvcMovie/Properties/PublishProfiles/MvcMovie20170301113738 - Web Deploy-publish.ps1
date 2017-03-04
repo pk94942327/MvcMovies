5b558704d3afb94e22bb9b80344e532d511920c5
TVDOMAIN\PetSjo <peter.sjostrand@tekniskaverken.se>
2017-03-04 09:55:25 +0100

    Publicerat på Azure kontot. Lyckad funktionalitet på GoogleChrome. Men inte på IE.

---
+++ src/MvcMovie/Properties/PublishProfiles/MvcMovie20170301113738 - Web Deploy-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = 'a2d2e9a8-e550-41c5-bb5c-2f917f8d0a93'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
